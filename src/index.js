4aa891e0d090e329f7b91d07b6aa89545beedec6
sagarp1405 <sagarhiremath1405@gmail.com>
2026-06-28 23:11:29 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -17,6 +17,16 @@ if (!process.env.MONGODB_URI) {
 }
 
 connectDB()
+.then(()=>{
+    app.listen (process.env.PORT||8000,()=>{
+        console.log(`Server is running at port:${
+            process.env.PORT
+        }`);
+    })
+})
+.catch((err)=>{
+        console.log("MONGO db connection failed !!!",err)
+})
 /*
 Approach 1
 import mongoose from "mongoose"
